242963bebe9c3982bc534908fc208634bd2e0bd2
mf11y <56240891+celiasaumell@users.noreply.github.com>
2022-11-07 21:40:46 -0600

    creater router, register routes

--- app/cmd/main/main.go
+++ app/cmd/main/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/celiasaumell/GoWebAPI/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	request1()
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Welcome to go web api")
-	fmt.Println("Endpoint hit: homepage")
-}
-
-func aboutMe(w http.ResponseWriter, r *http.Request) {
-	who := "CeliaSaumell"
-
-	fmt.Fprintf(w, "A little about CC")
-	fmt.Println("endpoint hit ", who)
-
-}
-
-func request1() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/aboutme", aboutMe)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	routes.RegisterRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
